2bd8af8830f9fb75e244d7f61ad33b5f35cf0d2a
Marcus Kastner <marcushkastner@gmail.com>
2022-09-03 13:24:12 -0700

    added products

---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim shirt',
+      slug: 'nike-slim-shirt',
+      catagory: 'Shirts',
+      image: '/images/p1.jpeg', //679 x 829
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      name: 'Adidas Fit Shirt',
+      slug: 'Adidas-fit-shirt',
+      catagory: 'Shirt',
+      image: '/images/p2.jpeg',
+      price: 250,
+      countInStock: 20,
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      name: 'Nike Slim Pant',
+      slug: 'nike-slim-pant',
+      catagory: 'Pants',
+      image: '/images/p3.jpeg',
+      price: 25,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product',
+    },
+    {
+      name: 'Adidas Fit Pant',
+      slug: 'Adidas-fit-pant',
+      catagory: 'Pants',
+      image: '/images/p4.jpeg',
+      price: 65,
+      countInStock: 5,
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+  ],
+};
+
+export default data;
