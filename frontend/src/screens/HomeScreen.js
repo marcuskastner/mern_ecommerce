35dc4bdde78d8d0245d461c715edc06780e8044c
Marcus Kastner <marcushkastner@gmail.com>
2022-09-03 17:55:17 -0700

    pulling data from backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,21 @@
 import { Link } from 'react-router-dom';
-import data from '../data';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchdata = async () => {
+      const result = await axios.get('api/products');
+      setProducts(result.data);
+    };
+    fetchdata();
+  }, []);
   return (
     <div>
       <h1>Feartured Products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div key={product.slug} className="product">
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
