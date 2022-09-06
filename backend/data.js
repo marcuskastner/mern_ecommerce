e02ec85b1b48f3210e677adb8e081ba6230aa8f9
Marcus Kastner <marcushkastner@gmail.com>
2022-09-05 22:19:54 -0700

    connected to mongoDB

--- backend/data.js
+++ backend/data.js
@@ -1,6 +1,7 @@
 const data = {
   products: [
     {
+      _id: '1',
       name: 'Nike Slim shirt',
       slug: 'nike-slim-shirt',
       catagory: 'Shirts',
@@ -13,18 +14,20 @@ const data = {
       description: 'high quality product',
     },
     {
+      _id: '2',
       name: 'Adidas Fit Shirt',
       slug: 'Adidas-fit-shirt',
       catagory: 'Shirt',
       image: '/images/p2.jpeg',
       price: 250,
-      countInStock: 20,
+      countInStock: 0,
       brand: 'Adidas',
       rating: 4.0,
       numReviews: 10,
       description: 'high quality product',
     },
     {
+      _id: '3',
       name: 'Nike Slim Pant',
       slug: 'nike-slim-pant',
       catagory: 'Pants',
@@ -37,6 +40,7 @@ const data = {
       description: 'high quality product',
     },
     {
+      _id: '4',
       name: 'Adidas Fit Pant',
       slug: 'Adidas-fit-pant',
       catagory: 'Pants',
